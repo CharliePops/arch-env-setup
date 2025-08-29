a624131c97cc95977525413a11bb16934b9bacd8
charlie <carlosbarrantesb@gmail.com>
2025-08-29 14:51:33 +0200

    create ssh keys if not existing

--- config.sh
+++ config.sh
@@ -4,10 +4,13 @@ git config --global user.email "$EMAIL"
 git config --global init.defaultBranch main
 git config --global core.editor "nvim"
 
-# Create ssh keys for github
-ssh-keygen -t ed25519 -C "$EMAIL" -f "$HOME/.ssh/id_ed25519" -N ""
-eval "$(ssh-agent -s)"
-ssh-add "$HOME/.ssh/id_ed25519"
+# Create ssh keys for github if they don't exist
+if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
+    echo "Creating SSH keys for GitHub..."
+    ssh-keygen -t ed25519 -C "$EMAIL" -f "$HOME/.ssh/id_ed25519" -N ""
+    eval "$(ssh-agent -s)"
+    ssh-add "$HOME/.ssh/id_ed25519"
+fi
 
 # Make bash the default shell
 echo "Making bash the default shell..."
